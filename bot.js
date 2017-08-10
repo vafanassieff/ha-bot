039b840c8bf324fdbd5f3cfb8a5c11214babf40b
Victor Afanassieff <victor@afanassieff.com>
2017-08-11 00:24:27 +0200

    [ADD] Testing with amazon

--- bot.js
+++ bot.js
@@ -7,35 +7,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+    client.user.setGame(`on ${client.guilds.size} servers`);
+    console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
 let responseObject = { //Message who will trigger the bot
-  "ah": "Denis te surveille",
-  "ha": "C'est pas bien de dire ah",
-  "a": "Mauvaise idee ici",
-  "hah": "Bien essayer Loic",
-  "<:ha:311864943047737344>": "Grosse emote"
+    "ah": "Denis te surveille",
+    "ha": "C'est pas bien de dire ah",
+    "a": "Mauvaise idee ici",
+    "hah": "Bien essayer Loic",
+    "<:ha:311864943047737344>": "Grosse emote"
 };
 
 client.on('message', message => {
+
     if(message.author.bot) //Avoid repeat
         return;
 
-    const channel = message.channel;
-    const msg = message.content.toLowerCase();
-
-    if (responseObject[message.content])
+    if (responseObject[message.content.toLocaleLowerCase()])
     {
         var ret = "<:ha:311864943047737344> ";
         ret += message.author;
-        channel.send(ret);
+        message.author.send(ret);
 
         if (message.member.voiceChannel) {
             message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
             
             const dispatcher = connection.playFile('./AH.mp3');
-            
+
             dispatcher.on('end', () => { //Disconnect the bot after playing the sound
                connection.disconnect();
             });
